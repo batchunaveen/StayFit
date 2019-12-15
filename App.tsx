81e53c6fd704600c9cf2b52db2d406ee34992d9d
Naveen <batchunaveen6@gmail.com>
2019-12-15 09:50:47 -0500

    updating project

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Stay Fit to Track Metrics!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
