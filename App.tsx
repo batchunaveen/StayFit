ba1dfa2e2dcc4c9879f81d9f41b658d356b34f7a
Naveen <batchunaveen6@gmail.com>
2019-12-16 20:01:53 -0500

    Adding Header to StayFit app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
+import { StyleSheet, Text,TouchableOpacity, TextStyle, View, ViewStyle } from 'react-native';
+
+
+
+class MyButton extends React.Component {
+  setNativeProps = nativeProps => {
+    this._root.setNativeProps(nativeProps);
+  };
+
+  render() {
+    return React.createElement(View, _extends({
+      ref: component => this._root = component
+    }, this.props), React.createElement(Text, null, this.props.label));
+  }
+
+}
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Stay Fit to Track Metrics!</Text>
-    </View>
-  );
+  return React.createElement(View, {style: headerStyles.container}, 
+  React.createElement(Header, {
+  placement: "center",
+  leftComponent: {
+    icon: 'menu',
+    color: '#fff'
+  },
+  centerComponent: {
+    text: 'StayFit',
+    style: {
+      color: '#fff'
+    }
+  },
+  rightComponent: {
+    icon: 'home',
+    color: '#fff'
+  }}));
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start'
+  }
+});
+
+const headerStyles = StyleSheet.create({ 
+  container: {
+    flex: 1, 
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'flex-start'
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # StayFit
 IOS App for running metrics
+
+
+## Steps to setup project 
+
+expo init 
+
+? Choose a template: expo-template-blank-typescript  
+? Yarn v1.10.1 found. Use Yarn to install dependencies? Yes
+To get started, you can type:
+
+  cd stayfit
+  yarn start
+
+Add jest
+  
